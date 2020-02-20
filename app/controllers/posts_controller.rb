b0c9767d6ae9f37b83de411e25395209dcb3287d
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-02-20 18:52:19 -0500

    infinite scroll feature finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,26 @@ class PostsController < ApplicationController
         @post = Post.create(content: params[:content], topic_id: params[:topic_id], user: @user)
         render json: @post
     end
+
+    def infinite_scroll
+        topic = Topic.all.find_by(slug: params[:slug])
+        @posts = posts_by_topic(topic).slice((8*params[:page].to_i) -8, second_argument )
+        render json: @posts
+    end
+
+    private 
+
+    def posts_by_topic(topic)
+        @posts = Post.all.where(topic_id: topic.id).reverse
+    end
+
+    def second_argument
+        if (8*params[:page].to_i) - 8 == 0
+            second_argument = 8*params[:page].to_i
+        elsif (8*params[:page].to_i) - 8 == 8
+            second_argument = (8*params[:page].to_i) - 8
+        elsif (8*params[:page].to_i) - 8 > 8
+            second_argument = (8*params[:page].to_i) - ((8*params[:page].to_i) - 8 )
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get '/persist', to: 'users#persist'
+  post '/login', to: 'users#login'
+
+  # get '/posts/spirituality/:page', to: 'posts#spirituality'
+  # get '/posts/love/:page', to: 'posts#love'
+  # get '/posts/discipline/:page', to: 'posts#discipline'
+  # get '/posts/philosophy/:page', to: 'posts#philosophy'
+  # get '/posts/positivity/:page', to: 'posts#positivity'
+
+  get '/posts/:slug/:page', to: 'posts#infinite_scroll'
   
   resources :users
   resources :likes
@@ -6,7 +16,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :topics
   
-  get '/persist', to: 'users#persist'
-  post '/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
