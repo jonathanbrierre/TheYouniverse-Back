e85c98387ac29244e705cd0e86c6c63f9cbd683f
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-02-21 22:07:00 -0500

    like button functionality added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+    before_action :authorized, only: [:create, :destroy]
+
+    def create 
+        # byebug
+        @like = Like.create( post_id: params[:post_id], user: @user)
+        if @like.valid? 
+            render json: @like
+        else
+            render json: {message: 'Like Failed - Try Again'}
+        end
+    end
+
+    def destroy
+        # byebug
+        @like = Like.find_by(id: params[:id])
+        @like.destroy
+        render json: {message: 'successful destruction'}
+    end
 end
--- app/serializers/like_serializer.rb
+++ app/serializers/like_serializer.rb
@@ -1,5 +1,5 @@
 class LikeSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user
   has_one :post
   has_one :user
 end
