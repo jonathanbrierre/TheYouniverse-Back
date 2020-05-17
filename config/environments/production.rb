f2eafe115884c704a4386c28564608bc02f4d182
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-05-16 21:29:38 -0400

    allow request origin wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = [%r{https?://\S+}]
+  config.action_cable.allowed_request_origins = [%r{http://*}, %r{https://*}, %r{ws://*}, %r{wss://*}]
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
