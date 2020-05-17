722fe9a6dfca69b7f1d5688078d9c79afe32ed68
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-05-16 21:26:20 -0400

    allow request origin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3001']
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.action_cable.allowed_request_origins = [%r{https?://\S+}]
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
@@ -50,3 +50,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
